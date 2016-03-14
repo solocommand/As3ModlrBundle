13d14491d72a62f27e9ec9246e9caf69c2edbe58
Jacob Bare <jacob.bare@cygnus.com>
2016-03-14 10:05:26 -0500

    Initial commit

---
+++ As3ModlrRestOdmBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace As3\Bundle\ModlrRestOdmBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class As3ModlrRestOdmBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
