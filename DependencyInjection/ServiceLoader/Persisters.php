49fd08ea1302c267eb71375c677481796ae5a033
Jacob Bare <jacob.bare@cygnus.com>
2016-03-21 11:21:01 -0500

    Add formatter and query definitions

--- DependencyInjection/ServiceLoader/Persisters.php
+++ DependencyInjection/ServiceLoader/Persisters.php
@@ -15,43 +15,6 @@ use Symfony\Component\DependencyInjection\Reference;
 class Persisters implements ServiceLoaderInterface
 {
     /**
-     * Creates the MongoDB persister service definition.
-     * Will also load support services.
-     *
-     * @param   string              $persisterName
-     * @param   array               $persisterConfig
-     * @param   ContainerBuilder    $container
-     * @return  Definition
-     */
-    private function createMongoDbPersister($persisterName, array $persisterConfig, ContainerBuilder $container)
-    {
-        // Storage metadata
-        $smfName = sprintf('%s.metadata', $persisterName);
-        $definition = new Definition(
-            Utility::getLibraryClass('Persister\MongoDb\StorageMetadataFactory'),
-            [new Reference(Utility::getAliasedName('util.entity'))]
-        );
-        $definition->setPublic(false);
-        $container->setDefinition($smfName, $definition);
-
-        // Connection
-        $conName = sprintf('%s.connection', $persisterName);
-        $options = isset($persisterConfig['parameters']['options']) && is_array($persisterConfig['parameters']['options']) ? $persisterConfig['parameters']['options'] : [];
-        $definition = new Definition(
-            'Doctrine\MongoDB\Connection',
-            [$persisterConfig['parameters']['host'], $options]
-        );
-        $definition->setPublic(false);
-        $container->setDefinition($conName, $definition);
-
-        // Persister
-        return new Definition(
-            Utility::getLibraryClass('Persister\MongoDb\Persister'),
-            [new Reference($conName), new Reference($smfName)]
-        );
-    }
-
-    /**
      * {@inheritdoc}
      */
     public function load(array $config, ContainerBuilder $container)
@@ -77,5 +40,98 @@ class Persisters implements ServiceLoaderInterface
             $managerDef->addMethodCall('addPersister', [new Reference($persisterName)]);
         }
         return $this;
+    }
+
+    /**
+     * Creates the connection service definition.
+     *
+     * @param   array   $persisterConfig
+     * @return  Definition
+     */
+    private function createConnection(array $persisterConfig)
+    {
+        $options = isset($persisterConfig['parameters']['options']) && is_array($persisterConfig['parameters']['options']) ? $persisterConfig['parameters']['options'] : [];
+        $definition = new Definition(
+            'Doctrine\MongoDB\Connection',
+            [$persisterConfig['parameters']['host'], $options]
+        );
+        $definition->setPublic(false);
+        return $definition;
+    }
+
+    /**
+     * Creates the persistence formatter service definition.
+     *
+     * @return  Definition
+     */
+    private function createFormatter()
+    {
+        $definition = new Definition(
+            Utility::getLibraryClass('Persister\MongoDb\Formatter')
+        );
+        $definition->setPublic(false);
+        return $definition;
+    }
+
+    /**
+     * Creates the MongoDB persister service definition.
+     * Will also load support services.
+     *
+     * @param   string              $persisterName
+     * @param   array               $persisterConfig
+     * @param   ContainerBuilder    $container
+     * @return  Definition
+     */
+    private function createMongoDbPersister($persisterName, array $persisterConfig, ContainerBuilder $container)
+    {
+        // Storage metadata
+        $smfName = sprintf('%s.metadata', $persisterName);
+        $definition = $this->createSmf();
+        $container->setDefinition($smfName, $definition);
+
+        // Connection
+        $conName = sprintf('%s.connection', $persisterName);
+        $definition = $this->createConnection($persisterConfig);
+        $container->setDefinition($conName, $definition);
+
+        // Formatter
+        $formatterName = sprintf('%s.formatter', $persisterName);
+        $definition = $this->createFormatter();
+        $container->setDefinition($formatterName, $definition);
+
+        // Query
+        $queryName = sprintf('%s.query', $persisterName);
+        $definition = $this->createQuery($conName, $formatterName);
+        $container->setDefinition($queryName, $definition);
+
+        // Persister
+        return new Definition(
+            Utility::getLibraryClass('Persister\MongoDb\Persister'),
+            [new Reference($queryName), new Reference($smfName)]
+        );
+    }
+
+    /**
+     * Creates the storage metadata factory service definition.
+     *
+     * @return  Definition
+     */
+    private function createSmf()
+    {
+        $definition = new Definition(
+            Utility::getLibraryClass('Persister\MongoDb\StorageMetadataFactory'),
+            [new Reference(Utility::getAliasedName('util.entity'))]
+        );
+        $definition->setPublic(false);
+        return $definition;
+    }
+
+    private function createQuery($conName, $formatterName)
+    {
+        $definition = new Definition(
+            Utility::getLibraryClass('Persister\MongoDb\Query'),
+            [new Reference($conName), new Reference($formatterName)]
+        );
+        return $definition;
     }
 }
